230b1d1d79234e17ca72572d8cfd8bc51467d008
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-06 18:45:16 +0300

    writes code for anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, arr):
+        matched_word = []
+        for word in arr:
+            if sorted(word) == sorted(self.word):
+                matched_word.append(word)
+        return matched_word
+
+
+listen = Anagram("listen")
+listen.match(["enlists", "google", "inlets", "banana"])
+
